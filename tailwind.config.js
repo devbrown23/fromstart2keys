a6f2b061251c5bb33e3aa1d41dad4bcdc5ec158b
devbrown23 <102295370+devbrown23@users.noreply.github.com>
2025-08-09 17:54:12 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        gold: {
+          500: '#FFD700', // main gold
+          600: '#E6C200', // darker hover gold
+        },
+        black: '#000000',
+      }
+    },
+  },
   plugins: [],
 }
